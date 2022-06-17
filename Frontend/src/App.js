118eb67556e2c5cd9abf2b02cd293c9bd4eb669b
Stan <stan2dor@gmail.com>
2022-06-17 10:01:09 -0700

    Folder structure(2)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./assets/logo.svg";
 import "./App.css";
 
 function App() {
@@ -6,7 +6,6 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-
         <h1>Kompweb</h1>
         <p> Online Solutions</p>
         {/* <p>
