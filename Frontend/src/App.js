6b1611285976f861e304fdea945c05fcbb9169c2
Stan <stan2dor@gmail.com>
2022-06-17 13:26:24 -0700

    ant design npm

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,12 +12,19 @@ function App() {
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
+        <form className='App-form'>
+          <label>
+            {/* Say 9! */}
+            <input type='text' name='name' />
+          </label>
+          <input type='submit' value='Submit' />
+        </form>
         <a
-          className='App-link'
-          href='/contact-us'
+          className='App-link-client'
+          href='/login'
           target='_blank'
           rel='noopener noreferrer'>
-          CONTACT
+          Client Log-in
         </a>
       </header>
     </div>
