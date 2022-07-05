ce0babaffc7c83d510bf23c03e354e1a0070f0dc
Stan <stan2dor@gmail.com>
2022-07-04 19:03:18 -0700

    navbar bug

--- Frontend/src/components/common/Navbar.js
+++ Frontend/src/components/common/Navbar.js
@@ -46,16 +46,16 @@ export default function Navbar() {
       >
         <ul>
           <li>
-            <a href="/home">Home</a>
+            <a href="/">Home</a>
           </li>
           <li>
-            <a href="/projects">Projects</a>
+            <a href="/projects/">Projects</a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a href="/about/">About</a>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <a href="/contact/">Contact</a>
           </li>
         </ul>
       </div>
