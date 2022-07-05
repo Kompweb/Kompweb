9bdb85ecb2a07798f4a7cbb62c863f590d2480d2
Stan <stan2dor@gmail.com>
2022-07-04 22:24:16 -0700

    style cleanup

--- Frontend/src/components/common/Navbar.js
+++ Frontend/src/components/common/Navbar.js
@@ -49,13 +49,19 @@ export default function Navbar() {
             <a href="/">Home</a>
           </li>
           <li>
-            <a href="/projects/">Projects</a>
+            <a aria-current="page" href="/projects/">
+              Projects
+            </a>
           </li>
           <li>
-            <a href="/about/">About</a>
+            <a aria-current="page" href="/about/">
+              About
+            </a>
           </li>
           <li>
-            <a href="/contact/">Contact</a>
+            <a aria-current="page" href="/contact/">
+              Contact
+            </a>
           </li>
         </ul>
       </div>
