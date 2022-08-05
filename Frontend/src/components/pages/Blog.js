f1d4c7afd548b8e90e173f99b29658a5bd9ee72e
Stan <stan2dor@gmail.com>
2022-08-05 16:23:22 -0700

    blog innitial commit

---
+++ Frontend/src/components/pages/Blog.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BugOutlined } from '@ant-design/icons';
+
+function Blog() {
+  return (
+    <div>
+      <h1>soon...</h1>
+      <BugOutlined
+        style={{
+          //   color: 'white',
+          fontSize: 64,
+          backgroundColor: '#61dafb',
+          borderRadius: '8px',
+        }}
+      />
+    </div>
+  );
+}
+
+export default Blog;
--- Frontend/src/components/pages/Projects.js
+++ Frontend/src/components/pages/Projects.js
@@ -5,19 +5,34 @@ import Languages from '../common/Languages';
 import PotShop from './AllProjects/PotShop';
 import ScribleStadium from './AllProjects/ScribleStadium';
 import CryptoApp from './AllProjects/CryptoApp';
+import Framer from '../common/FrameMotion';
+import { DashboardOutlined } from '@ant-design/icons';
+
 function Projects() {
   return (
     <div className="projects">
-      {/* <h1 style={{ color: '#fff' }}>Projects</h1> */}
-      <PotShop />
-      <ScribleStadium />
-      <CryptoApp />
-      <p>
+      {/* <h1>Projects</h1> */}
+      <div className="projects-list">
+        <PotShop />
+        <ScribleStadium />
+        <CryptoApp />
+      </div>
+      <Framer />
+      <div className="bottom-container">
+        <h2>Online Dashboards</h2>
+        <p>
+          Anytime and anywhere, our customers can track their progress to see
+          when theyll reach their next milestone.
+        </p>
+        <DashboardOutlined className="icons" />
+
+        {/* <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industrys standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book.
-      </p>
+      </p> */}
+      </div>
       <Languages />
       {/* <SearchForm /> */}
     </div>
