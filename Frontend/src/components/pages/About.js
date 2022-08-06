7da5d8d4adf43962cc2e9927f9387f76ff74955a
Stan <stan2dor@gmail.com>
2022-08-06 09:32:59 -0700

    clean-up style

--- Frontend/src/components/pages/About.js
+++ Frontend/src/components/pages/About.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../../styles/about.css';
 import { DownCircleOutlined } from '@ant-design/icons';
-
 // import Framer from '../common/FrameMotion';
-// import SearchForm from '../common/SearchForm';
 
 export default function AboutUs() {
   return (
     <div className="about-us">
-      <h1>About US</h1>
+      <h1>About us</h1>
       <p>
         Kompweb US-based React developers have the expertise required to help
         organizations consistently deliver mission-critical applications at the
@@ -19,17 +17,11 @@ export default function AboutUs() {
       </a>
       <br />
       <a href="/projects/"> Explore Projects</a>
-
-      {/* <Link to="/projects">Explore Projects</Link> */}
-
       {/* <Framer /> */}
       <div className="b-content">
         <h3>Partner with Kompweb</h3>
-        <h4>We guarantee the success of your React project</h4>
+        <p>We guarantee the success of your React project</p>
       </div>
-      {/* <Framer /> */}
-
-      {/* <SearchForm className="search-form" /> */}
     </div>
   );
 }
