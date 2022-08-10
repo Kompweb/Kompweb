76eb33b4d12b68fee36f2c693f9f6d73a4dc8703
Stan <stan2dor@gmail.com>
2022-08-09 23:13:37 -0700

    coingeico API innitial commit

--- Frontend/src/components/pages/AllProjects/CryptoApp.js
+++ Frontend/src/components/pages/AllProjects/CryptoApp.js
@@ -1,41 +1,29 @@
 import { Row, Card } from 'antd';
-
 import React from 'react';
-// import '../../styles/Shop.less';
-// import '../../styles/Shop.css';
-// import Modal from '../../common/Modal';
-
-// const DemoBox = props => (
-//   <p className={`height-${props.value}`}>{props.children}</p>
-// );
+import { Link } from 'react-router-dom';
 
 const { Meta } = Card;
-// const onMenuClick = e => {
-//   console.log('click', e);
-// };
+
 const CryptoApp = () => (
   <>
     {/* <Modal /> */}
     <div className="shop-container">
       <Row justify="center" align="top">
         {/* <Col span={4}> */}
-        <Card
-          hoverable
-          style={{ width: 240 }}
-          cover={
-            <img
-              alt="example"
-              src={require('../../../assets/crypto-wallet.png')}
-            />
-          }
-        >
-          <Meta title="Blockchain" description="" />
-        </Card>
-        {/* <DemoBox value={280}> */}
-        {/* <a style={{}}>more info</a> */}
-        {/* </DemoBox> */}
-        {/* <p>Description</p> */}
-        {/* </Col> */}
+        <Link to={'/'}>
+          <Card
+            hoverable
+            // style={{ width: 240 }}
+            cover={
+              <img
+                alt="example"
+                src={require('../../../assets/crypto-wallet.png')}
+              />
+            }
+          >
+            <Meta title="Blockchain" description="" />
+          </Card>
+        </Link>
       </Row>
     </div>
   </>
